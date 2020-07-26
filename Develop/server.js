7ee30bac5a722425f45981c92a7c57641f6f2db5
Andrew Hall <andrewbhall96@gmail.com>
2020-07-25 21:56:11 -0400

    SC and deleted unnessessary lines

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const mongoose  = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = requrie("./models");
+const db = require("./models");
 
 const app = express();
 
@@ -17,13 +17,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
-db.User.create({ name: "Ernest Hemingway" })
-  .then(dbUser => {
-    console.log(dbUser);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
 
   app.get("/exercise", (req,res) => {
       db.Exercise.find({})
