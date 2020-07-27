180cb57fd49f656a7e880383a2e14a04e5e6af0c
Andrew Hall <andrewbhall96@gmail.com>
2020-07-26 21:13:21 -0400

    added post request

--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,8 @@ const db = require("./models");
 
 const app = express();
 
+const path = require("path");
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -19,25 +21,57 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trackerdb", { u
 
 
 app.get("/exercise/new", (req, res) => {
-  db.Workout.find({})
-    .then(dbExercise => {
-      res.json(dbExercise);
-    })
-    .catch(err => {
-      res.json(err);
+  // db.Workout.find({})
+  //   .then(dbExercise => {
+      res.sendFile(path.join(__dirname + '/public/exercise.html'));
     });
+    // .catch(err => {
+    //   res.json(err);
+    // });
+// });
+// db.notes.insert(req.body (err, data) => {
+//   if(err) {
+//     console.log(err)
+//   } else {
+
+//   }
+// })
+// res.json.body;
+// })
+
+
+app.post("/exercise/new", (req, res) => {
+  db.Workout.insert(req.body, (err, data) => {
+    if(err) {
+      console.log(err)
+    } else {
+
+    }
+  })
+  res.json.body;
 });
 
 // Let's examine this
 app.get("/exercise/continue", (req, res) => {
   db.Workout.find().sort({day:-1}).limit(1)
     .then(dbContinue => {
-      res.json(dbContinue);
+      res.sendFile(__dirname + '/public/exercise.html');
     })
     .catch(err => {
       res.json(err);
     });
 })
+
+app.post("/exercise/continue", (req, res) => {
+  db.Workout.update({_id: req.body._id}, (err, data) => {
+    if(err) {
+      console.log(err)
+    } else {
+
+    }
+  })
+  res.json.body;
+});
 
 
 app.listen(PORT, () => {
